733a6b7a12b1523cac5dec781f7592da1889a58f
Alexandr <91833054+AlexandrTerehin@users.noreply.github.com>
2024-03-08 17:37:04 +0400

    Добавление хранилища для user

--- app/src/main/java/com/example/papino/App.kt
+++ app/src/main/java/com/example/papino/App.kt
@@ -1,7 +1,16 @@
 package com.example.papino
 
 import android.app.Application
+import com.example.papino.net.User
 
 class App : Application() {
 
+    private var user: User? = null
+
+
+    fun setUser(user: User) {
+        this.user = user
+    }
+
+    fun getUser() = user
 }
