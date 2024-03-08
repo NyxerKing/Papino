55488812f8575e5cbc28116064a8b62507ce9541
Alexandr <91833054+AlexandrTerehin@users.noreply.github.com>
2024-03-08 23:08:06 +0400

    Фикс использование новыз компонентов

--- uikit/src/main/java/ru/papino/uikit/components/cards/CardMenuBase.kt
+++ uikit/src/main/java/ru/papino/uikit/components/cards/CardMenuBase.kt
@@ -1,9 +1,9 @@
 package ru.papino.uikit.components.cards
 
 import android.content.Context
-import android.content.res.ColorStateList
 import android.net.Uri
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -37,7 +37,19 @@ class CardMenuBase @JvmOverloads constructor(
     private val textTitle = findViewById<TextView>(R.id.textTitle)
     private val textSubtitle = findViewById<TextView>(R.id.textSubtitle)
     private val textPrice = findViewById<TextView>(R.id.textPrice)
+    private val textPriceCount = findViewById<TextView>(R.id.textPriceCount)
     private val buttonAddCart = findViewById<Button>(R.id.buttonAddCart)
+
+    init {
+        this.radius = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            CORNER_RADIUS,
+            context.resources.displayMetrics
+        )
+
+        setCardBackgroundColor(resources.getColor(R.color.backgroundCardColor, context.theme))
+        strokeWidth = 0
+    }
 
     /**
      * Установить параметры
@@ -47,6 +59,8 @@ class CardMenuBase @JvmOverloads constructor(
      * @param title заголовок
      * @param subtitle описание
      * @param price цена
+     * @param priceCount текст о измерении и сумме
+     * @param buttonText текст на кнопке
      * @param buttonColor фон кнопки
      * @param buttonOnClick коллбэк на кнопку
      */
@@ -56,6 +70,8 @@ class CardMenuBase @JvmOverloads constructor(
         title: String? = null,
         subtitle: String? = null,
         price: String? = null,
+        priceCount: String? = null,
+        buttonText: String? = null,
         @ColorRes buttonColor: Int? = null,
         buttonOnClick: (() -> Unit)? = null,
     ) {
@@ -70,11 +86,18 @@ class CardMenuBase @JvmOverloads constructor(
         title?.let { textTitle.text = it }
         subtitle?.let { textSubtitle.text = it }
         price?.let { textPrice.text = it }
+        priceCount?.let { textPriceCount.text = it }
+
+        buttonText?.let { buttonAddCart.text = it }
 
         buttonColor?.let { colorInt ->
-            buttonAddCart.backgroundTintList = ColorStateList.valueOf(colorInt)
+            buttonAddCart.setBackgroundColor(resources.getColor(colorInt, context.theme))
         }
 
         buttonOnClick?.let { click -> buttonAddCart.setOnClickListener { click() } }
+    }
+
+    companion object {
+        private const val CORNER_RADIUS = 10f
     }
 }
