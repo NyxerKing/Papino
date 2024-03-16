4fbb7b25838c0a47e63cabc0fcf4ab751745a389
aaterekhin <terehinaa@vtb.ru>
2024-03-16 20:44:06 +0400

    Изменение в корзине

--- restaurant/src/main/java/ru/papino/restaurant/extensions/BasketExtensions.kt
+++
@@ -1,6 +0,0 @@
-package ru.papino.restaurant.extensions
-
-import com.google.gson.Gson
-import ru.papino.restaurant.presentation.basket.models.BasketUIModel
-
-internal fun BasketUIModel.toJson() = Gson().toJson(this)
